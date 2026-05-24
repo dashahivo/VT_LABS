14c4337e6bc042e2d348f08797ea3b3375b20a1e
Darya Marmysh <dashkmarm@gmail.com>
2026-05-24 19:31:17 +0300

    Add LAB05 Windows Forms Calculator

---
+++ LAB05/LAB05/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LAB05
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Число 1:";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(271, 56);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(228, 22);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Число 2:";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(271, 98);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(228, 22);
+            this.txtNumber2.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(162, 146);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(243, 146);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 23);
+            this.btnSub.TabIndex = 5;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(324, 146);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 23);
+            this.btnMul.TabIndex = 6;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(405, 146);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 23);
+            this.btnDiv.TabIndex = 7;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(243, 187);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(156, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(159, 238);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(80, 16);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор V1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ LAB05/LAB05/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = GetNumber1();
+                double num2 = GetNumber2();
+                double result = num1 + num2;
+                ShowResult(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        // Кнопка "-"
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = GetNumber1();
+                double num2 = GetNumber2();
+                double result = num1 - num2;
+                ShowResult(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        // Кнопка "*"
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = GetNumber1();
+                double num2 = GetNumber2();
+                double result = num1 * num2;
+                ShowResult(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        // Кнопка "/"
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = GetNumber1();
+                double num2 = GetNumber2();
+
+                if (num2 == 0)
+                {
+                    ShowError("Ошибка: Деление на ноль!");
+                    return;
+                }
+
+                double result = num1 / num2;
+                ShowResult(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        // Кнопка "Очистить"
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumber1.Text = "";
+            txtNumber2.Text = "";
+            lblResult.Text = "Результат:";
+            lblResult.ForeColor = System.Drawing.Color.Black;
+        }
+
+        // Получить первое число
+        private double GetNumber1()
+        {
+            if (string.IsNullOrWhiteSpace(txtNumber1.Text))
+                throw new Exception("Ошибка: Введите первое число!");
+
+            if (!double.TryParse(txtNumber1.Text, out double result))
+                throw new Exception("Ошибка: Первое число должно быть числом!");
+
+            return result;
+        }
+
+        // Получить второе число
+        private double GetNumber2()
+        {
+            if (string.IsNullOrWhiteSpace(txtNumber2.Text))
+                throw new Exception("Ошибка: Введите второе число!");
+
+            if (!double.TryParse(txtNumber2.Text, out double result))
+                throw new Exception("Ошибка: Второе число должно быть числом!");
+
+            return result;
+        }
+
+        // Показать результат
+        private void ShowResult(string result)
+        {
+            lblResult.Text = $"Результат: {result}";
+            lblResult.ForeColor = System.Drawing.Color.Black;
+        }
+
+        // Показать ошибку (красным цветом)
+        private void ShowError(string errorMessage)
+        {
+            lblResult.Text = errorMessage;
+            lblResult.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}
